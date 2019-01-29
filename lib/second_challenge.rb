f2d5a39b1ce50c1e268ac652046bfaffbccf2312
Kate <katekim5809@gmail.com>
2019-01-29 00:19:40 +0000

    Done.

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,11 +1,19 @@
 def second_challenge
   epic_tragedy = {
-    :montague => {
+    :montague => {}
+    :patriarch => {},
+    :matriarch => {},
+    :hero => {},
+    :heroine_friends =>[]
       #your key/value pairs here!
-      }, 
-    :capulet => {
+      },
+    :capulet => {}
+    :patriarch => {},
+    :matriarch => {},
+    :hero => {},
+    :heroine_friends =>[]
       #your key/value pairs here!
       }}
 
-  
+
 end
